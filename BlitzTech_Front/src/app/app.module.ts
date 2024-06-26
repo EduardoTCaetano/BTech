9c703f512aec9df721252da33f054fa95623686e
Eduardo Timossi Caetano <eduardotimossi1101@hotmail.com>
2024-06-26 00:08:54 -0300

    Add components

--- BlitzTech_Front/src/app/app.module.ts
+++ BlitzTech_Front/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { AdvertisingComponent } from './components/advertising/advertising.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    IntroductionComponent,
+    CardsComponent,
+    AdvertisingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ BlitzTech_Front/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+  }
+
+}
